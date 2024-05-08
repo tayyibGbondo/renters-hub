9325ee693d550e7acdf5e6f428a46d508e73b88d
Tayyib Gbondo <gbondotahib72@gmail.com>
2024-05-08 13:13:57 +0000

    Installatoion of the react complete, innstalled Tailwindcss and write the first hello template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-extrabold text-3xl bg-green-800 text-white' >Helo</h1>
+    </>
+  )
+}
+
+export default App
