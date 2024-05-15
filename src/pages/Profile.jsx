76e5ac619689c7b842467368edbd95d1b65f1224
Tayyib Gbondo <gbondotahib72@gmail.com>
2024-05-15 11:48:42 +0000

    logout functionality workingnow

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,34 +1,66 @@
 import { getAuth } from "firebase/auth";
 import React, { useState } from "react";
+import { useNavigate } from "react-router";
 
 export default function Profile() {
-  const auth = getAuth()
+  //Handling useNavigate
+  const navigaate = useNavigate();
 
-  const[formData, setFormData]=useState({
+  const auth = getAuth();
+
+  const [formData, setFormData] = useState({
     name: "Tayyib Gbondo",
     email: "test@test.com",
-  })
+  });
 
-  const {name, email} = formData
+  const { name, email } = formData;
 
-  return <>
-  <section className="max-w-6xl mx-auto flex flex-col justify-center items-center">
-    <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
+  //Logout functionality
+  const logout = () => {
+    const auth = getAuth();
+    auth.signOut();
+    navigaate("/")
+  };
 
-    <div className=" w-full md:w-[50%] px-3">
-      <form>
-        {/* name input */}
-        <input type="text" id="name" value={name} disabled className="mb-6 w-full px-4 text-xl text-gray-700 py-2 transition ease-in-out bg-white rounded border border-gray-300" />
+  return (
+    <>
+      <section className="max-w-6xl mx-auto flex flex-col justify-center items-center">
+        <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
 
-        {/* email input */}
-        <input type="email" id="email" value={email} disabled className="mb-6 w-full px-4 text-xl text-gray-700 py-2 transition ease-in-out bg-white rounded border border-gray-300" />
+        <div className=" w-full md:w-[50%] px-3">
+          <form>
+            {/* name input */}
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled
+              className="mb-6 w-full px-4 text-xl text-gray-700 py-2 transition ease-in-out bg-white rounded border border-gray-300"
+            />
 
-        <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
-          <p>Do you want to change your name? <span className="text-red-600 hover:text-red-800 transition ease-in-out duration-200 ml-1">Edit</span></p>
-          <p className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200">Sign out</p>
+            {/* email input */}
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled
+              className="mb-6 w-full px-4 text-xl text-gray-700 py-2 transition ease-in-out bg-white rounded border border-gray-300"
+            />
+
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p>
+                Do you want to change your name?{" "}
+                <span className="text-red-600 hover:text-red-800 transition ease-in-out duration-200 ml-1">
+                  Edit
+                </span>
+              </p>
+              <p onClick={logout} className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer">
+                Sign out
+              </p>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
-  </section>
-  </>;
+      </section>
+    </>
+  );
 }
